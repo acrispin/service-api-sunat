b77550e041a2b086f40d756fa3f4388548c8e8b0
Antonio Crispin <antonio.crispin@gmail.com>
2019-12-03 12:47:10 -0500

    add service sunat

---
+++ src/main/java/com/unicon/api/serviceapisunat/controller/SunatController.java
@@ -0,0 +1,43 @@
+package com.unicon.api.serviceapisunat.controller;
+
+import com.unicon.api.serviceapisunat.beans.EmpresaSunatBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class SunatController {
+
+    private static Logger log = LoggerFactory.getLogger(SunatController.class);
+
+    @GetMapping("")
+    public EmpresaSunatBean getEmpresaByRuc(@RequestParam(defaultValue = "") String ruc) {
+        log.info("listar empresa por ruc: " + ruc);
+        if (ruc == null || ruc.isEmpty())
+        {
+            return new EmpresaSunatBean();
+        }
+        EmpresaSunatBean empresa = new EmpresaSunatBean(
+                "20272209325",
+                "PESCA PERU MOLLENDO S.A.",
+                "BAJA DEFINITIVA",
+                "HABIDO",
+                "150122"
+        );
+        empresa.setTipoVia("AV.");
+        empresa.setNombreVia("JOSE PARDO");
+        empresa.setCodigoZona("");
+        empresa.setTipoZona("");
+        empresa.setNumeroDomicilio("601");
+        empresa.setInteriorDomicilio("1603");
+        empresa.setLote("");
+        empresa.setNumeroDepartamento("");
+        empresa.setManzana("");
+        empresa.setKilometro("");
+        return empresa;
+    }
+}
